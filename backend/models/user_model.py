4eb0d80f5ce69ab8e56436a2f2acd79bb226e5df
AbdelRahman <115741665+AbdelRahmanShahy@users.noreply.github.com>
2025-02-03 12:11:43 +0200

    Add files via upload

---
+++ backend/models/user_model.py
@@ -0,0 +1,15 @@
+from database import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+users_collection = db["users"]
+
+def create_user(email, password):
+    hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
+    users_collection.insert_one({"email": email, "password": hashed_password})
+    return True
+
+def find_user_by_email(email):
+    return users_collection.find_one({"email": email})
+
+def verify_password(stored_password, provided_password):
+    return check_password_hash(stored_password, provided_password)
