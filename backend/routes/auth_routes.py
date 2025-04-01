64788a1b60ceeda214f016c7f976741e59cea2ea
Alsaead Aziz <alsaeedaziz323@gmail.com>
2025-04-02 00:50:48 +0200

    handle unused files

--- backend/routes/auth_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from controllers.auth_controller import signup_controller, login_controller, logout_controller
-
-auth_routes = Blueprint("auth_routes", __name__)
-
-auth_routes.route("/api/signup", methods=["POST"])(signup_controller)
-auth_routes.route("/api/login", methods=["POST"])(login_controller)
-auth_routes.route("/api/logout", methods=["POST"])(logout_controller)
--- backend/routes/fortillm_results_routes.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from controllers.fortillm_results_controller import receive_results_controller
-
-fortillm_results_routes = Blueprint("fortillm_results_routes", __name__)
-
-fortillm_results_routes.route("/api/fortillm_results", methods=["POST"])(receive_results_controller)
